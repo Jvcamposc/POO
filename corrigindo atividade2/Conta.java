f39788209ef768d6a5e3a49f9dab00c94b337edf
João Victor Campos Costa <137218085+Jvcamposc@users.noreply.github.com>
2024-09-04 09:49:05 -0300

    Add files via upload

---
+++ corrigindo atividade2/Conta.java
@@ -0,0 +1,40 @@
+public class Conta
+{
+	private int numero;
+	private double saldo, limite;
+
+	public Conta(int numero, double saldo, double limite)
+ 	{
+	this.numero = numero;
+	this.saldo = saldo;
+	this.limite = limite;
+ 	}
+
+	public void setSaldo(double valor)
+	{
+ 	this.saldo += valor;
+	}
+
+	public double getSaldo()
+	{
+	return this.saldo;
+	}
+
+	public void sacar(double valor)
+	{
+	if(valor <  this.limite)
+	this.saldo -= valor;
+	else
+	System.out.println("Saldo insuficiente!");
+    	}
+
+	public void setNumero(int numero){
+	this.numero = numero;
+	}
+	public int getNumero()
+	{
+
+	return this.numero;
+
+	}
+}
