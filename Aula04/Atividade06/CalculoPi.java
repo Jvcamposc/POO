1fd477934908677742de65cf49a928468277ac46
Jvcamposc <jvcamposc@gmail.com>
2024-09-18 22:30:55 -0300

    Numero PRIMO / Calculando PI

--- Aula04/Atividade06/CalculoPi.java
+++ Aula04/Atividade06/CalculoPi.java
@@ -5,6 +5,25 @@ package Aula04.Atividade06;
 
 public class CalculoPi {
     public static void main(String[] args) {
-        
+        double pi = 0.0;
+        int termosUsados = 0;
+        double alvo = 3.14159;
+
+        System.out.printf("Termo \t Aproximação\n");
+
+        // Calcula Pi com uma série diferente
+        for (int i = 0; i < 200000; i++) {
+            pi += (i % 2 == 0 ? 4.0 : -4.0) / (2 * i + 1);
+            if (i % 5000 == 0) {
+                System.out.printf("%d \t %.5f\n", i, pi);
+            }
+            if (Math.abs(pi - alvo) < 0.00001) {
+                termosUsados = i;
+                break;
+            }
+        }
+
+        System.out.println("--- Resultado ---");
+        System.out.printf("O valor de Pi (%.5f) foi encontrado em %d termos!\n", alvo, termosUsados);
     }
 }
--- Aula04/Atividade13/numPrimo.java
+++ Aula04/Atividade13/numPrimo.java
@@ -1,3 +1,5 @@
+/*Faça um aplicativo que verifique se o número digitado n é um número primo. Lembrando que todo número
+primeiro é divisível apenas por 1 ou por ele mesmo. */
 package Aula04.Atividade13;
 import java.util.Scanner;
 public class numPrimo {
@@ -7,25 +9,21 @@ public class numPrimo {
         System.out.print("Digite um número inteiro: ");
         int n = input.nextInt();
 
-        // Números menores que 2 não são primos
         if (n < 2) {
             System.out.println(n + " não é primo.");
             input.close();
             return;
         }
 
-        boolean isPrimo = true; // Vamos supor que n é primo
-
-        // Verifica se n é divisível por qualquer número de 2 até n-1
+        boolean Primo = true; 
         for (int i = 2; i < n; i++) {
             if (n % i == 0) {
-                isPrimo = false; // Encontrou um divisor
-                break; // Não precisa continuar
+                Primo = false; 
+                break; 
             }
         }
-
         // Resultado
-        if (isPrimo) {
+        if (Primo) {
             System.out.println(n + " é primo.");
         } else {
             System.out.println(n + " não é primo.");
