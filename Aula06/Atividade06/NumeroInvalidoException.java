2662886b392f5d1dbab138ad308a008910d92b62
Jvcamposc <jvcamposc@gmail.com>
2024-10-29 07:44:34 -0300

    Salvar o numero do usuario

--- Aula06/Atividade06/NumeroInvalidoException.java
+++ Aula06/Atividade06/NumeroInvalidoException.java
@@ -1,3 +1,5 @@
+/*Crie um programa que solicite ao usuário um número entre 1 e 100. Use uma assertiva para
+verificar se o número está dentro do intervalo. Caso contrário, o programa deve lançar um erro. */
 package Aula06.Atividade06;
 
 public class NumeroInvalidoException extends Exception {
