306041444c4116d0d11704220adde1683c5330d6
Jvcamposc <jvcamposc@gmail.com>
2024-10-29 07:43:20 -0300

    Salvar numero do usuario

---
+++ Aula06/Atividade06/Main.java
@@ -0,0 +1,27 @@
+package Aula06.Atividade06;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Digite um número entre 1 e 100: ");
+        int numero = scanner.nextInt();
+
+        try {
+            verificarNumero(numero);
+            System.out.println("Você digitou o número: " + numero);
+        } catch (NumeroInvalidoException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+
+        scanner.close();
+    }
+
+    public static void verificarNumero(int numero) throws NumeroInvalidoException {
+        if (numero < 1 || numero > 100) {
+            throw new NumeroInvalidoException("O número deve estar entre 1 e 100.");
+        }
+    }
+}
